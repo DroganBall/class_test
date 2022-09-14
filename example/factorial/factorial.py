92fd9f8c95b96771a04ca6a7728b20aeeef28544
OohelloworldoO <sunny1234860@gmail.com>
2022-09-14 17:38:15 +0800

    logfactorial

--- example/factorial/factorial.py
+++ example/factorial/factorial.py
@@ -1,8 +1,20 @@
+import math
+
 def factorial(n):
     r = 1
     for i in range(1,n+1):
         r *= i
     return r
 
+def logfactorial(n):
+    r = 0
+    for i in range(1,n+1):
+        r += math.log(i)
+        return r
+
+
 if __name__ == '__main__':
     print("factorial(10)=",factorial(10))
+    print('logfactorial(10)=', math.exp(logfactorial(10)))
+    print('exp(logfactorial(10))=', math.exp(logfactorial(10)))
+    assert round(math.exp(logfactorial(10)))==factorial(10)
--- example/factorial/test_factorial.py
+++ example/factorial/test_factorial.py
@@ -1,3 +1,7 @@
 from factorial import *
+
 def test_factorial():
     assert factorial(5) == 120
+
+def test_logfactorial():
+    assert factorial(5) == round(math.exp(logfactorial(5)))
